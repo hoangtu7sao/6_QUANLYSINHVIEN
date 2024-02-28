4f4400905dff1cabc9647be82754cfa295154041
hien <ngvanhien0308@gmail.com>
2024-02-28 21:41:34 +0700

    Add project files.

---
+++ 6_QUANLYSINHVIEN/frmQuanLySinhVien.Designer.cs
@@ -0,0 +1,309 @@
+﻿
+namespace _6_QUANLYSINHVIEN
+{
+    partial class frmQuanLySinhVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lbMaSV = new System.Windows.Forms.Label();
+            this.lbHoTen = new System.Windows.Forms.Label();
+            this.btthem = new System.Windows.Forms.Button();
+            this.btclose = new System.Windows.Forms.Button();
+            this.btclear = new System.Windows.Forms.Button();
+            this.btRight = new System.Windows.Forms.Button();
+            this.btLeft = new System.Windows.Forms.Button();
+            this.btDoubleRight = new System.Windows.Forms.Button();
+            this.DoubleLeft = new System.Windows.Forms.Button();
+            this.txtmasv = new System.Windows.Forms.TextBox();
+            this.txthoten = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lbDSSV = new System.Windows.Forms.Label();
+            this.btSearch = new System.Windows.Forms.Button();
+            this.lbDSDaChon = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 7;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.Controls.Add(this.btRight, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btLeft, 3, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btDoubleRight, 3, 5);
+            this.tableLayoutPanel1.Controls.Add(this.DoubleLeft, 3, 6);
+            this.tableLayoutPanel1.Controls.Add(this.lbDSSV, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btSearch, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lbDSDaChon, 5, 2);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView2, 4, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btthem, 4, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btclear, 5, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btclose, 6, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txthoten, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lbHoTen, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtmasv, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lbMaSV, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(684, 355);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lbMaSV
+            // 
+            this.lbMaSV.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lbMaSV.AutoSize = true;
+            this.lbMaSV.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMaSV.Location = new System.Drawing.Point(3, 40);
+            this.lbMaSV.Name = "lbMaSV";
+            this.lbMaSV.Size = new System.Drawing.Size(43, 15);
+            this.lbMaSV.TabIndex = 0;
+            this.lbMaSV.Text = "Mã SV";
+            this.lbMaSV.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbHoTen
+            // 
+            this.lbHoTen.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lbHoTen.AutoSize = true;
+            this.lbHoTen.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbHoTen.Location = new System.Drawing.Point(197, 40);
+            this.lbHoTen.Name = "lbHoTen";
+            this.lbHoTen.Size = new System.Drawing.Size(43, 15);
+            this.lbHoTen.TabIndex = 1;
+            this.lbHoTen.Text = "Họ tên";
+            this.lbHoTen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btthem
+            // 
+            this.btthem.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btthem.Location = new System.Drawing.Point(399, 36);
+            this.btthem.Name = "btthem";
+            this.btthem.Size = new System.Drawing.Size(75, 23);
+            this.btthem.TabIndex = 2;
+            this.btthem.Text = "Cập nhật";
+            this.btthem.UseVisualStyleBackColor = true;
+            // 
+            // btclose
+            // 
+            this.btclose.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btclose.Location = new System.Drawing.Point(595, 36);
+            this.btclose.Name = "btclose";
+            this.btclose.Size = new System.Drawing.Size(75, 23);
+            this.btclose.TabIndex = 3;
+            this.btclose.Text = "Thoát";
+            this.btclose.UseVisualStyleBackColor = true;
+            // 
+            // btclear
+            // 
+            this.btclear.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btclear.Location = new System.Drawing.Point(496, 36);
+            this.btclear.Name = "btclear";
+            this.btclear.Size = new System.Drawing.Size(75, 23);
+            this.btclear.TabIndex = 4;
+            this.btclear.Text = "Xóa";
+            this.btclear.UseVisualStyleBackColor = true;
+            // 
+            // btRight
+            // 
+            this.btRight.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btRight.Location = new System.Drawing.Point(302, 99);
+            this.btRight.Name = "btRight";
+            this.btRight.Size = new System.Drawing.Size(75, 23);
+            this.btRight.TabIndex = 5;
+            this.btRight.Text = ">";
+            this.btRight.UseVisualStyleBackColor = true;
+            // 
+            // btLeft
+            // 
+            this.btLeft.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btLeft.Location = new System.Drawing.Point(302, 163);
+            this.btLeft.Name = "btLeft";
+            this.btLeft.Size = new System.Drawing.Size(75, 23);
+            this.btLeft.TabIndex = 6;
+            this.btLeft.Text = "<";
+            this.btLeft.UseVisualStyleBackColor = true;
+            // 
+            // btDoubleRight
+            // 
+            this.btDoubleRight.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btDoubleRight.Location = new System.Drawing.Point(302, 227);
+            this.btDoubleRight.Name = "btDoubleRight";
+            this.btDoubleRight.Size = new System.Drawing.Size(75, 23);
+            this.btDoubleRight.TabIndex = 7;
+            this.btDoubleRight.Text = ">>";
+            this.btDoubleRight.UseVisualStyleBackColor = true;
+            // 
+            // DoubleLeft
+            // 
+            this.DoubleLeft.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.DoubleLeft.Location = new System.Drawing.Point(302, 291);
+            this.DoubleLeft.Name = "DoubleLeft";
+            this.DoubleLeft.Size = new System.Drawing.Size(75, 23);
+            this.DoubleLeft.TabIndex = 8;
+            this.DoubleLeft.Text = "<<";
+            this.DoubleLeft.UseVisualStyleBackColor = true;
+            // 
+            // txtmasv
+            // 
+            this.txtmasv.Location = new System.Drawing.Point(100, 35);
+            this.txtmasv.Name = "txtmasv";
+            this.txtmasv.Size = new System.Drawing.Size(91, 20);
+            this.txtmasv.TabIndex = 9;
+            // 
+            // txthoten
+            // 
+            this.txthoten.Location = new System.Drawing.Point(294, 35);
+            this.txthoten.Name = "txthoten";
+            this.txthoten.Size = new System.Drawing.Size(90, 20);
+            this.txthoten.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox3, 2);
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox3.Location = new System.Drawing.Point(294, 67);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(188, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // lbDSSV
+            // 
+            this.lbDSSV.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.lbDSSV, 2);
+            this.lbDSSV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbDSSV.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDSSV.Location = new System.Drawing.Point(3, 64);
+            this.lbDSSV.Name = "lbDSSV";
+            this.lbDSSV.Size = new System.Drawing.Size(188, 32);
+            this.lbDSSV.TabIndex = 12;
+            this.lbDSSV.Text = "Danh sách sinh viên";
+            this.lbDSSV.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btSearch
+            // 
+            this.btSearch.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btSearch.Location = new System.Drawing.Point(205, 68);
+            this.btSearch.Name = "btSearch";
+            this.btSearch.Size = new System.Drawing.Size(75, 23);
+            this.btSearch.TabIndex = 13;
+            this.btSearch.Text = "Tìm kiếm";
+            this.btSearch.UseVisualStyleBackColor = true;
+            // 
+            // lbDSDaChon
+            // 
+            this.lbDSDaChon.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.lbDSDaChon, 2);
+            this.lbDSDaChon.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbDSDaChon.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDSDaChon.Location = new System.Drawing.Point(488, 64);
+            this.lbDSDaChon.Name = "lbDSDaChon";
+            this.lbDSDaChon.Size = new System.Drawing.Size(193, 32);
+            this.lbDSDaChon.TabIndex = 14;
+            this.lbDSDaChon.Text = "Danh sách đã chọn";
+            this.lbDSDaChon.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView1, 3);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.tableLayoutPanel1.SetRowSpan(this.dataGridView1, 4);
+            this.dataGridView1.Size = new System.Drawing.Size(285, 253);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView2, 3);
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(391, 99);
+            this.dataGridView2.Name = "dataGridView2";
+            this.tableLayoutPanel1.SetRowSpan(this.dataGridView2, 4);
+            this.dataGridView2.Size = new System.Drawing.Size(290, 253);
+            this.dataGridView2.TabIndex = 16;
+            // 
+            // frmQuanLySinhVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 355);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "frmQuanLySinhVien";
+            this.Text = "frmQuanLySinhVien";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lbHoTen;
+        private System.Windows.Forms.Label lbMaSV;
+        private System.Windows.Forms.Button btthem;
+        private System.Windows.Forms.Button btclear;
+        private System.Windows.Forms.Button btclose;
+        private System.Windows.Forms.Button btRight;
+        private System.Windows.Forms.Button btLeft;
+        private System.Windows.Forms.Button btDoubleRight;
+        private System.Windows.Forms.Button DoubleLeft;
+        private System.Windows.Forms.TextBox txtmasv;
+        private System.Windows.Forms.TextBox txthoten;
+        private System.Windows.Forms.Label lbDSSV;
+        private System.Windows.Forms.Button btSearch;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lbDSDaChon;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
